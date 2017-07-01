e170ea6ab9ebaf13df112cc38df4ae0d1739e66d
Andre Hendriks <weze10bj@kpnmail.nl>
2017-07-01 22:09:08 +0200

    dlllibrary

---
+++ ExtIORTLSDR2832/ExtIORTLSDR2832/DLLLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtIORTLSDR2832
+{
+    public class DLLLibrary
+    {
+        DLLibrary()
+        {
+
+        }
+
+        public static FARPROC GetProcAddress(LPCSTR functionName)
+        {
+
+        }
+        public static HINSTANCE GetHinstance()
+        {
+
+        }
+        private Load(object dllName)
+        {
+            return;
+        } 
+        private void Load(object wstring, dllName)
+        {
+
+        } 
+        private void Free()
+        {
+
+        }
+    }
+}
