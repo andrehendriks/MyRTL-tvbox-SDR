23c23fc4f0f886f2e5a61a33a584e6f838c180a0
Andre Hendriks <weze10bj@kpnmail.nl>
2017-07-01 08:35:17 +0200

    Agc Added

---
+++ ExtIORTLSDR2832/ExtIORTLSDR2832/AutomaticGainControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtIORTLSDR2832
+{
+    public class AutomaticGainControl
+    {
+        Agc()
+        {
+
+        }
+        private void GetBackwardGainSpeed()
+        {
+
+        }
+        private void GetForwardGainSpeed()
+        {
+
+        }
+        private void GetMaxGaindB()
+        {
+
+        }
+        private void ComputeOutput(object input)
+        {
+            MaxGaindB = input + ForwardGainSpeed / BackwardGainSpeed;/*50.0=input+6.0/3.0*/
+        }
+        private void Reset()
+        {
+
+        }
+        private void SetBackwardGainSpeed(object db_per_second)
+        {
+            db_per_second = 2.0;
+        }
+        private void SetForwardGainSpeed(object db_per_second)
+        {
+            db_per_second = 4.0;
+        }
+        private void SetMaxGaindB(object gain_dB)
+        {
+            gain_dB = 50.0;
+        }
+        double ForwardGainSpeed=6.0;
+        double BackwardGainSpeed=3.0;
+        double MaxGaindB=50.0;
+    }
+}
